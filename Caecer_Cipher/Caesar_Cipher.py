c2a1ae51c890d79c45cc011fab7f0777c977d209
Allando <alla2634@edu.easj.dk>
2017-01-14 00:20:24 +0100

    User input

--- Caecer_Cipher/Caesar_Cipher.py
+++ Caecer_Cipher/Caesar_Cipher.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python3
 """
-code from https://www.youtube.com/watch?v=vPpRkHUPX_Q
+Based on code from https://www.youtube.com/watch?v=vPpRkHUPX_Q
 """
 
 import string
 import collections
+
+
+message = input("Enter you message: ")
+key = input("Enter the key: ")
+try:
+    validate = int(key)
+except ValueError:
+    print("Error: The key must be an int:")
 
 
 def caesar(rotate_string, number_to_rotate_by):
@@ -19,8 +27,9 @@ def caesar(rotate_string, number_to_rotate_by):
     lower = ''.join(list(lower))
 
     return rotate_string.translate(str.maketrans(string.ascii_uppercase, upper)).\
-        translate(str.maketrans(string.ascii_lowercase,lower))
+        translate(str.maketrans(string.ascii_lowercase, lower))
 
 
-print(caesar("Hello, World", 13))
+print(caesar(message, int(key)))
+
 
