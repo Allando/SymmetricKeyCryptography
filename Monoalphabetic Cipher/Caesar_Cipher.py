af1fbf8c64b7a35f6566a76c2a1acc60074d26fa
Allando <alla2634@edu.easj.dk>
2017-01-19 09:38:05 +0100

    user input

--- Monoalphabetic Cipher/Caesar_Cipher.py
+++ Monoalphabetic Cipher/Caesar_Cipher.py
@@ -6,17 +6,24 @@ Based on code from https://www.youtube.com/watch?v=vPpRkHUPX_Q
 import string
 import collections
 
+mode = input("Would you like to encrypt(e) or decrypt(d)?: ")
+message = ""
+key = 1
 
-message = input("Enter you message: ")
-key = input("Enter the key: ")
-try:
-    validate = int(key)
-except ValueError:
-    print("Error: The key must be an int:")
+
+def user_input(message, key):
+    message = input("Enter the message: ")
+    key = input("Enter the key: ")
+
+    try:
+        validate = int(key)
+    except ValueError:
+        print("Error: The key must be an int:")
+
+    return message, key
 
 
 def caesar(rotate_string, number_to_rotate_by):
-
     upper = collections.deque(string.ascii_uppercase)
     lower = collections.deque(string.ascii_lowercase)
 
@@ -26,10 +33,19 @@ def caesar(rotate_string, number_to_rotate_by):
     upper = ''.join(list(upper))
     lower = ''.join(list(lower))
 
-    return rotate_string.translate(str.maketrans(string.ascii_uppercase, upper)).\
+    return rotate_string.translate(str.maketrans(string.ascii_uppercase, upper)). \
         translate(str.maketrans(string.ascii_lowercase, lower))
 
 
-print(caesar(message, int(key)))
+if mode == "E" or "e":
+    user_input(message, key)
+    key = int(key) * -1
+    # print(caesar(message, int(key)))
+elif mode == "D" or "d":
+    user_input(message, key)
+    # print(caesar(message, int(key)))
+else:
+    print("Error")
 
 
+print("Encrypted\n ", caesar(message, int(key)))
